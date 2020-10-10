bca62d7f8582935bd1d31b2cf918424273a8ed98
Peter Kim <tspeterkim3@gmail.com>
2020-10-10 12:28:13 +0000

    analyze large data

--- notebooks/autopilot_data.py
+++ notebooks/autopilot_data.py
@@ -1,21 +1,28 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[5]:
+# In[1]:
 
 
 import os
 import sys
 
 
-# In[8]:
+# In[2]:
 
 
-# TODO: sys.path hack to call stuff up a directory
+sys.path.append("..") # Adds higher directory to python modules path. # Works for EC2
+
+
+# In[ ]:
+
+
+# TODO: sys.path hack to call stuff up a directory 
+# This works for Mac
 sys.path.insert(1, os.path.join(sys.path[0], '..'))
 
 
-# In[28]:
+# In[3]:
 
 
 import pandas as pd
@@ -23,50 +30,80 @@ import seaborn as sns
 import numpy as np
 
 
-# In[7]:
+# In[4]:
 
 
 from dataset import DrivingDataset
 
 
+# In[5]:
+
+
+data_path = '2020-10-04_06:22:49'
+
+
+# In[6]:
+
+
+train_path, val_path = f'../data/{data_path}/train', f'../data/{data_path}/val'
+
+
 # In[12]:
 
 
-train_path, val_path = '../data/2020-10-03_14:11:32/train', '../data/2020-10-03_14:11:32/val'
+train_set, val_set = DrivingDataset(train_path), DrivingDataset(val_path)
 
 
-# In[13]:
-
-
-train_set = DrivingDataset(train_path)
-
-
-# In[14]:
+# In[ ]:
 
 
 train_set.labels
 
 
-# In[15]:
+# In[8]:
 
 
 label_df = pd.DataFrame.from_dict(train_set.labels, orient='index')
 
 
-# In[19]:
+# In[ ]:
 
 
 label_df['steer']
 
 
-# In[34]:
+# In[9]:
 
 
 max(label_df['steer']), min(label_df['steer'])
 
 
-# In[32]:
+# In[10]:
 
 
 sns.displot(label_df['steer'], bins=np.arange(-1, 1.001, 0.05))
+
+
+# In[13]:
+
+
+label_df = pd.DataFrame.from_dict(val_set.labels, orient='index')
+
+
+# In[14]:
+
+
+max(label_df['steer']), min(label_df['steer'])
+
+
+# In[15]:
+
+
+sns.displot(label_df['steer'], bins=np.arange(-1, 1.001, 0.05))
+
+
+# In[ ]:
+
+
+
 
