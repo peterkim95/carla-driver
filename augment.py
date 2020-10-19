1dbcf64c987f4ae0284020af62275d27c9ebb5ae
Peter Kim <tspeterkim3@gmail.com>
2020-10-19 17:47:02 +0900

    flip corrective steering sign

--- augment.py
+++ augment.py
@@ -7,6 +7,6 @@ def translate_img(img, steering_angle, x_offset_limit, y_offset_limit):
     ty = y_offset_limit * np.random.uniform(-1, 1)
 
     timg = img.transform(img.size, Image.AFFINE, (1,0,tx,0,1,ty), Image.BICUBIC)
-    tangle = steering_angle + (0.004 * tx)
+    tangle = steering_angle + (-0.004 * tx)
 
     return timg, tangle
